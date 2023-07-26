08b996be273a0945acbcd5e54b3c7488c367d432
Kamrul Bari <69103450+kamrulbari14@users.noreply.github.com>
2023-07-26 18:35:43 +0600

    [strategy] get enum values through an API

--- strategy-design-pattern-spring/src/main/java/com/example/strategydesignpatternspring/controller/PaymentController.java
+++ strategy-design-pattern-spring/src/main/java/com/example/strategydesignpatternspring/controller/PaymentController.java
@@ -3,6 +3,7 @@ package com.example.strategydesignpatternspring.controller;
 import com.example.strategydesignpatternspring.enums.PaymentMethod;
 import com.example.strategydesignpatternspring.factory.PaymentFactory;
 import com.example.strategydesignpatternspring.payment.Payment;
+import java.util.Arrays;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +19,12 @@ public class PaymentController {
   public String payWith(@PathVariable PaymentMethod method) {
     Payment payment = factory.getMethod(method);
     return payment.getMessage();
+  }
+
+  @GetMapping("/get-enum")
+  public PaymentMethod[] getEnum() {
+    Arrays.stream(PaymentMethod.values()).forEach(System.out::println);
+    return PaymentMethod.values();
   }
 
 }
